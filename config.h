425001b7e12633fa22957ab008f3e7adf7e13634
deishuukaiki <maciek54134@tutanota.com>
2022-04-15 11:41:53 +0200

    initial commit

---
+++ config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"SauceCodePro Nerd Font:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#dcd7ba", "#1f1f28" },
+	[SchemeSel] = { "#dcd7ba", "#664d77" },
+	[SchemeOut] = { "#dcd7ba", "#658594" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
